61ebdc24171adaaa86c8ab2949ece056e71895c7
ShernOC <sherlynea1994@hotmail.com>
2024-11-29 16:33:31 +0300

    list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(num):
+    return [n for n in num if n % 2 == 0]
+    # make_list = [ n for n in n range(1,11)]
+num_list = range(1,10,2)
+
+print(return_evens(num_list))
 
 def make_exclamation(sentence_list):
-    pass
+
+    return [n + "!" for n in sentence_list]
+
+
+
+print (make_exclamation("I like computers"))
